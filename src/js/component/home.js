14e43c6ea05a39f7e7df052ab2008ce3c8cb7d81
silvinavd <silvinavalverde8@gmail.com>
2021-04-29 12:48:37 +0000

    Semaforo done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,9 +1,42 @@
-import React from "react";
-import { Color } from "./lightcolor.js";
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
-	return <Color />;
+	const [color, setColor] = useState("");
+
+	return (
+		<div className="bg-dark width">
+			<div className="d-flex align-items-center flex-column bd-highlight mb-3">
+				<button
+					className={
+						"btn btn-danger btn-lg rounded-circle m-1 " +
+						(color == "red" ? "selected" : "")
+					}
+					onClick={() => setColor("red")}
+					type="button">
+					{"R"}
+				</button>
+
+				<button
+					className={
+						"btn btn-warning btn-lg rounded-circle m-1 " +
+						(color == "yellow" ? "selected" : "")
+					}
+					onClick={() => setColor("yellow")}
+					type="button">
+					A{/*<i class="fas fa-exclamation"></i>*/}
+				</button>
+				<button
+					className={
+						"btn btn-success btn-lg rounded-circle m-1 " +
+						(color == "green" ? "selected" : "")
+					}
+					onClick={() => setColor("green")}
+					type="button">
+					{/*<i class="fas fa-walking"></i>*/}V
+				</button>
+				<div className="barra"></div>
+			</div>
+		</div>
+	);
 }
